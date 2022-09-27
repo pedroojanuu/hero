157fd9f5b71a963db2ecc1bd2121e0c084adb4b3
Pedro Januário <up202108768@edu.fe.up.pt>
2022-09-27 10:22:25 +0100

    Passo 5

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,38 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.print("Hello World!!!!");
-    }
-}
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
 
-public class Screen {
-    
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+
+        screen.clear();
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X') [0]);
+        screen.refresh();
+    }
 }
