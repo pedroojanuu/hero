58ae81a63be6d327b71adbc37c37003e799bb930
Pedro Januário <up202108768@edu.fe.up.pt>
2022-10-04 09:57:12 +0100

    Passo 7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +11,8 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
     public Game() {
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20);
@@ -19,20 +23,33 @@ public class Game {
             screen.setCursorPosition(null);
             screen.startScreen();
             screen.doResizeIfNecessary();
-
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X') [0]);
-            screen.refresh();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X') [0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X') [0]);
         screen.refresh();
     }
     public void run() throws IOException {
-        draw();
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                screen.close();
+            } else if (key.getKeyType() == KeyType.EOF) {
+                break;
+            }
+            processKey(key);
+        }
+    }
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowUp: y--; break;
+            case ArrowRight: x++; break;
+            case ArrowDown: y++; break;
+            case ArrowLeft: x--; break;
+        }
     }
 }
