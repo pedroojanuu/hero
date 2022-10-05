772a0cfe3d2a336666cc544afc0bc2470a47b976
Pedro Januário <up202108768@edu.fe.up.pt>
2022-10-05 11:31:34 +0100

    Passo 12

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -3,22 +3,39 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arena {
     private int width;
     private int height;
     private Hero hero;
+    private List<Wall> walls;
     public Arena(int width, int height) {
         this.width = width;
         this.height = height;
         hero = new Hero(10, 10);
+        this.walls = createWalls();
+    }
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
     }
     public int getWidth() {return width;}
     public int getHeight() {return height;}
     private boolean canHeroMove(Position position) {
-        if (position.getX() > width - 1) return false;
-        if (position.getY() > height - 1) return false;
-        if (position.getX() < 0) return false;
-        if (position.getY() < 0) return false;
+        if (position.getX() > width - 2) return false;
+        if (position.getY() > height - 2) return false;
+        if (position.getX() < 1) return false;
+        if (position.getY() < 1) return false;
         return true;
     }
     private void moveHero(Position position) {
@@ -28,6 +45,8 @@ public class Arena {
     public void draw(TextGraphics graphics) {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        for (Wall wall : walls)
+            wall.draw(graphics);
         hero.draw(graphics);
     }
     public void processKey(KeyStroke key) {
---
+++ src/main/java/Wall.java
@@ -0,0 +1,26 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import org.w3c.dom.Text;
+
+public class Wall {
+    private Position position;
+    public Wall(int x, int y) {
+        position = new Position(x, y);
+    }
+    public Position getPosition() {
+        return position;
+    }
+    public void setX(int x) {
+        position.setX(x);
+    }
+    public void setY(int y) {
+        position.setY(y);
+    }
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "+");
+    }
+}
